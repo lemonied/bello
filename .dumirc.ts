53b10048ec244788f37dbb454d5fc47c4f7ed962
ChenJiYuan <447334358@qq.com>
2023-12-28 05:46:24 +0000

    feat: 修改publicPath

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   outputPath: 'docs-dist',
   base: '/bello/',
+  publicPath: '/bello/',
   themeConfig: {
     name: 'bello',
     editLink: false,
